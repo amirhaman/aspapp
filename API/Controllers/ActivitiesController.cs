30c9fffaad294a6cfffae4f29842dc2d80039a55
AmirNavid <amir@brinkinteractive.com>
2024-04-30 14:20:38 -0400

    added Create controller to the Activities API

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -10,7 +10,6 @@ namespace API.Controllers
     public class ActivitiesController : BaseApiController
     {   
 
-
         [HttpGet] //api/activities
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
@@ -20,6 +19,13 @@ namespace API.Controllers
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
             return await Mediator.Send(new Details.Query{Id = id});
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            await Mediator.Send(new Create.Command {Activity = activity});
+            
+            return Ok();
         }
     }
 }
